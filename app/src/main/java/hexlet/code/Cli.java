2d0519736a30c4d6ee09bf61efc9708ce3e1877f
varya.b <b.vrvra@gmail.com>
2025-02-22 18:28:44 +0300

    add Makefile and Whatsyourname

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,17 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void Greetings() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+        scanner.close();
+    }
+}
