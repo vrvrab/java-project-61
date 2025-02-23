f902c5595db98f8638d64af025b2d3321d55e0cf
varya.b <b.vrvra@gmail.com>
2025-02-23 12:32:56 +0300

    add Greet and Exit menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,26 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.\n" +
+                "1 - Greet\n" +
+                "0 - Exit");
 
-        Cli.greetings();
+        String gameNumber = scanner.next();
+        System.out.println("Your choice: " + gameNumber);
+
+        switch (gameNumber) {
+            case "0":
+                break;
+            case "1":
+                Cli.greetings();
+
+
+        }
+
+
     }
 }
