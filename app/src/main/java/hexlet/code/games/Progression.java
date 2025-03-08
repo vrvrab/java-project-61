1e33c9ce5fb7225ad21e9baf57f5f308b1a5d170
varya.b <b.vrvra@gmail.com>
2025-03-08 20:48:46 +0300

    fix Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -8,7 +8,7 @@ public class Progression {
     public static final int MIN_ADDITION_INT = 2;
     public static final int MAX_ADDITION_INT_BOUND = 50;
     public static final int MIN_HIDDEN_INDEX = 1;
-    public static final int MAX_HIDDEN_INDEX_BOUND = 200;
+    public static final int NUMBER_OF_ITEMS_IN_QUESTION = 10;
 
     public static String getRules() {
         return "What number is missing in the progression?";
@@ -19,12 +19,11 @@ public class Progression {
         for (int i = 0; i < rounds; i++) {
             Random rand = new Random();
             int addition = rand.nextInt(MIN_ADDITION_INT, MAX_ADDITION_INT_BOUND);
-            int indexOfHiddenElement = rand.nextInt(MIN_HIDDEN_INDEX, MAX_HIDDEN_INDEX_BOUND);
-            int numberOfElementsInRow = 10;
-            int current = rand.nextInt(MIN_FIRST_INT, MAX_FIRST_INT_BOUND);
+            int indexOfHiddenElement = rand.nextInt(MIN_HIDDEN_INDEX, NUMBER_OF_ITEMS_IN_QUESTION - 1);
+            int current = rand.nextInt(MIN_FIRST_INT, NUMBER_OF_ITEMS_IN_QUESTION - 1);
 
             qAndA[i][0] = String.valueOf(current);
-            for (int j = 1; j < numberOfElementsInRow; j++) {
+            for (int j = 1; j < NUMBER_OF_ITEMS_IN_QUESTION; j++) {
                 current += addition;
                 if (j == indexOfHiddenElement) {
                     qAndA[i][1] = String.valueOf(current);
